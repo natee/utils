b6803616343018b1ee97605af9f305148832aa43
zhaoke <kerncink@gmail.com>
2018-08-16 10:47:04 +0800

    数组稳定排序

---
+++ stable-sort.js
@@ -0,0 +1,25 @@
+/**
+ * stable-sort.js  稳定排序（Array.prototype.sort是非稳定排序）
+ * @example
+ *
+ * const a = [2,1,3,4,3]
+ * const b = stableSort(a)
+ * // => [1,2,3,3,4]
+ */
+// 可以把stableSort绑到Array.prototype上，并增加处理函数，
+// 但是我不想这样做，适时而定
+
+const stableSort = arr => {
+  return arr.map( (v, i) => {
+    return [v, i]
+  }).sort( (a, b) => {
+    if(a[0] === b[0]){
+      return a[1] - b[1]
+    }
+    return a[0] - b[0]
+  }).map( v => {
+    return v[0]
+  })
+}
+
+export default stableSort
